6a68c09769fbe98ef0c1e147a1badd7f72476f15
Kruz Kalke <kruzkalke@hotmail.com>
2014-11-16 21:12:53 -0600

    simple views implementation

---
+++ rango/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+def index(request) :
+	return HttpResponse("Rango says: Hello world! <a href='/rango/about'>About</a>")
+def about(request) :
+	return HttpResponse("""<a href="/rango/">Index</a>""")
